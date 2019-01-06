999f3ef9e7879f6f48a7a10c11333db4f81aef7c
梅志威 <meizhiwei@tezign.com>
2019-01-06 17:11:05 +0800

    可运行版

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import {Geolocation} from '@ionic-native/geolocation';
+
 import { MyApp } from './app';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
@@ -27,6 +29,7 @@ import { LoginPage } from '../pages/login/login';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  constructor(
+    public navCtrl: NavController,
+    public geolocation: Geolocation,
+    public toast: ToastController
+  ) { }
 
+  public getGps() {
+    console.warn('start geo --------');
+    this.geolocation.getCurrentPosition().then(res => {
+      console.log('geo ok', res);
+
+      // if (res && res.coords) {
+      //   alert(res.coords.longitude);
+      //   alert(res.coords.latitude);
+      // }
+      this.toast.create({
+        position: 'top',
+        duration:3000,
+        message: `定位成功啦，${res.coords.longitude}，${res.coords.latitude}，`
+      }).present();
+
+
+    },err=>{
+      console.log('geo err', err);
+    })
+  }
 }
