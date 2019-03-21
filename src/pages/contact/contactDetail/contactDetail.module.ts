b012e468ae2251849d83298373452ea13f6b45e4
梅志威 <meizhiwei@tezign.com>
2019-03-21 19:41:14 +0800

    加群聊

--- src/pages/contact/contactDetail/contactDetail.module.ts
+++ src/pages/contact/contactDetail/contactDetail.module.ts
@@ -5,13 +5,14 @@ import { LoadingService } from "../../../service/loading.service";
 import { ToastService } from "../../../service/toast.service";
 import { ConfirmService } from "../../../service/confirm.service";
 import { UserNetwork } from "../../../network/user.network";
+import { ChatNetwork } from "../../../network/chat.network";
 
 @NgModule({
   declarations: [ContactDetailPage],
   imports: [
     IonicPageModule.forChild(ContactDetailPage),
   ],
-  providers: [LoadingService, ToastService, ConfirmService,UserNetwork],
+  providers: [LoadingService, ToastService, ConfirmService, UserNetwork, ChatNetwork],
   entryComponents: [ContactDetailPage]
 })
 export class ContactDetailPageModule { }
--- src/pages/contact/contactDetail/contactDetail.ts
+++ src/pages/contact/contactDetail/contactDetail.ts
@@ -1,6 +1,8 @@
+import { uniq } from 'lodash';
 import { Component } from '@angular/core';
 import { IonicPage, NavParams, NavController } from 'ionic-angular';
 import { StorageService, STORAGE_KEY } from '../../../service/storage.service';
+import { ChatNetwork } from '../../../network/chat.network';
 
 @IonicPage({
   name: 'app-contact-contactDetail',
@@ -11,20 +13,16 @@ import { StorageService, STORAGE_KEY } from '../../../service/storage.service';
   templateUrl: 'contactDetail.html'
 })
 export class ContactDetailPage {
-
-
   contact: any = {};
-
-
   constructor(
     public params: NavParams,
     public navCtrl: NavController,
-    public storage: StorageService
+    public storage: StorageService,
+    public chatNetwork: ChatNetwork
   ) {
     this.contact = params.data.contact || {};
     console.log(params.data.contact);
   }
-
   sendMessage() {
     let userInfo = this.storage.get(STORAGE_KEY.USER_INFO);
     this.navCtrl.push('app-message-chat',
@@ -41,5 +39,16 @@ export class ContactDetailPage {
       }
     );
   }
-
+  sendGroupMessage() {
+    let userInfo = this.storage.get(STORAGE_KEY.USER_INFO);
+    let memberIds = [2, 63, 64, 65];
+    memberIds.push(userInfo.id);
+    this.chatNetwork.createGroup({
+      groupName: '五人组',
+      desc: '这是一个最好的时代，也是最坏的时代。',
+      memberIds: uniq(memberIds)
+    }).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
